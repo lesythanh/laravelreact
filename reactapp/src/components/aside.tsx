3013416d049699a3ec07abdcbe7fbabd96dc3a84
lesythanh <lesythanh063@gmail.com>
2025-05-10 10:34:56 +0700

    vd20

--- reactapp/src/components/aside.tsx
+++ reactapp/src/components/aside.tsx
@@ -10,9 +10,30 @@ import '../assets/scss/Accordion.scss'
 import '../assets/scss/Aside.scss'
 import { Link } from "react-router-dom";
 import { sidebarItem } from "../constant/sidebar";
+import { useEffect } from "react";
 
 
 const Aside = () => {
+
+    const location = useLocation();
+    const segments = location.pathname.split('/')[1];
+
+    const getOpenAccordionValue = () => {
+        for (let index = 0; index < sidebarItem.length; index++) {
+            const element = sidebarItem[index];
+            
+            for (let subIndex = 0; subIndex < element.items.length; subIndex++) {
+                const subElement = element.items[subIndex];
+                if (subElement.active.includes(segments)) {
+                    return `item-${subIndex}-${index}`;
+                }
+            }
+            
+        }
+    }
+    const defaultValue = getOpenAccordionValue();
+        
+
     return (
         <aside className="app-aside w-60 bg-[#111c43] h-full fixed">
             <div className="main-sidebar-header w-60 p-3.5 fixed z-10 h-14 text-center border-solid border-b border-menu-border">
@@ -26,11 +47,11 @@ const Aside = () => {
                         <div className="menu-category px-6 py-3 text-[#a3aed1] text-10px tracking-wider font-semibold opacity-50">
                             {item.label}
                         </div>
-                        <Accordion type="single" collapsible className="px-3 sidebar-accordion">
+                        <Accordion type="single" collapsible className="px-3 sidebar-accordion" defaultValue={defaultValue}>
                             {item.items.map((subItem, subIndex) => (
                                 <AccordionItem key={subIndex} value={`item-${subIndex}`}>
-                                    <AccordionTrigger className="text-[#a3aed1] rounded-lg bg-[rgba(255, 255, 255, .05)]">
-                                        <div className="menu-label flex flex-1 items-center">
+                                    <AccordionTrigger className={`rounded-lg ${subItem.active.includes(segments) ? 'text-[#a3aed1] bg-[rgba(255,255,255,.05)]' : ''}`}>
+                                        <div className={`menu-label flex flex-1 items-center text-white ${ subItem.active.includes(segments) ? 'text-white' : ''}`}>
                                             {subItem.icon}
                                             <span>{subItem.lable}</span>
                                         </div>
@@ -53,36 +74,6 @@ const Aside = () => {
                     </div>
                 ))
             }
-
-                {/* <div className="menu-category px-6 py-3 text-[#a3aed1] text-10px tracking-wider font-semibold opacity-50">
-                    MAIN
-                </div>
-                <Accordion type="single" collapsible className="px-3 sidebar-accordion">
-                    <AccordionItem value="item-1">
-                        <AccordionTrigger className="text-[#a3aed1] rounded-lg bg-[rgba(255, 255, 255, .05)]">
-                            <div className="menu-label flex flex-1 items-center">
-                                <FaHome className="text-sm mr-5 " />
-                                <span>Dashboard</span>
-                            </div>
-                        </AccordionTrigger>
-                        <AccordionContent className="border-0 mt-2">
-                            <ul>
-                                <li className="pl-6">
-                                    <Link className="side-menu__item block text-[#a3aed1] text-13px relative hover:text-white rounded-lg hover:bg-[rgba(255, 255, 255, .05)]" to="/dashboard">
-                                        Thống kê chung
-                                        <span className="absolute left-2 top-1/2 transform -translate-y-1/2 w-1 h-1 border border-solid border-primary rounded-full border-white"></span>
-                                    </Link>
-                                </li>
-                                <li className="pl-6">
-                                    <Link className="side-menu__item block text-[#a3aed1] text-13px relative hover:text-white rounded-lg hover:bg-[rgba(255, 255, 255, .05)]" to="/dashboard/order">
-                                        Thống kê đơn hàng
-                                        <span className="absolute left-2 top-1/2 transform -translate-y-1/2 w-1 h-1 border border-solid border-primary rounded-full border-white"></span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion> */}
             </div>
 
         </aside>
